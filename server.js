80827254e86bc04e1c05bf56426d2279e6d0008c
Shashank Chinmulgund <shashankchinmulgund@Shashanks-MacBook-Pro.local>
2017-04-17 18:42:39 -0700

    Initial commit

---
+++ server.js
@@ -0,0 +1,63 @@
+const express = require('express');
+const hbs = require('hbs');
+const fs = require('fs');
+
+var app = express();
+
+hbs.registerPartials(__dirname + '/views/partials');
+app.set('view engine', 'hbs');
+
+//setup log file
+app.use((req, res, next) => {
+  var now = new Date().toString();
+  var log = `${now}: ${req.method} ${req.url}`;
+
+  console.log(log);
+  fs.appendFile('server.log', log + '\n', (error) => {
+    if (error) {
+      console.log('Unable to append to file server.log.');
+    }
+  });
+  next();
+});
+
+// app.use((req, res, next)=>{
+//   res.render('maintenance.hbs');
+// });
+
+app.use(express.static(__dirname + '/public'));
+
+hbs.registerHelper('getCurrentYear', () => {
+  return new Date().getFullYear();
+  //return 'test';
+});
+
+hbs.registerHelper('screamIt', (text)=>{
+  return text.toUpperCase();
+});
+
+app.get('/', (req,res) => {
+  res.render('home.hbs',{
+    welcomeMessage: 'Welcome to the homepage of my first nodejs application',
+    pageTitle: 'Home page',
+    currentYear: new Date().getFullYear()
+  });
+});
+
+app.get('/about', (req, res) => {
+  //res.send('About the page');
+  res.render('about.hbs', {
+    pageTitle: 'About page',
+    currentYear: new Date().getFullYear()
+  });
+});
+
+app.get('/bad', (req, res) => {
+  res.send({
+    errorMessage: 'Hey! You have the required error message.'
+  });
+});
+
+app.listen(3000, () => {
+  console.log('Server is up and running on port 3000');
+});
